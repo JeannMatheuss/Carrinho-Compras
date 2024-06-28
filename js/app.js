e480b922c970641a746f11955dc2dd083bbfc12a
Jean <jeanmatheus0612@gmail.com>
2024-06-28 14:54:16 -0300

    Calculando o valor do produto

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 function adicionar() {
     // recuperar valores nome do produto, quantidade e valor
-    let nomePorduto = document.getElementById('produto');
+    let roduto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto);
+    alert(valorUnitario);
+    alert(quantidade.value);
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
 
 
 
