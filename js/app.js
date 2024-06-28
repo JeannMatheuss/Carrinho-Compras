61b2e1857a0a15323156e9587918a25096a04605
Jean <jeanmatheus0612@gmail.com>
2024-06-28 14:47:43 -0300

    Criando as funções no JS

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+    // recuperar valores nome do produto, quantidade e valor
+    let nomePorduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+
+
+
+    // calcular o preço, o nosso subtotal
+    // adicionar no carrinho
+    // atualizar o subtotal
+}
+
+function limpar() {
+
+}
